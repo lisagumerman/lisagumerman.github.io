a942abd0d2e798960e79b7804e5ad671eaa29785
Lisa Gumerman <lisajgumerman@gmail.com>
2014-09-27 12:37:42 -0600

    added drummage & archive slides

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
- $('body').on(click, 'a', function(){
-     var section = $(this).attr('id');
-     popup("#" + section + "-popup");
- }
-$('body').on(click, '.close, #fade', function(){
-     popup(".active-popup");
- }
-}               
-                  
-function popup(section) {
-     $("#fade").fadeToggle();
-     if ( $(section).hasClass("active-popup"){
-         $(section).attr('aria-hidden',true).removeClass("active-popup");
-         }
-    else { $(section).attr('aria-hidden',false).addClass("active-popup");
-}
+//$('body').on(click, 'a', function(){
+//     var section = $(this).attr('id');
+//     popup("#" + section + "-popup");
+//}
+//$('body').on(click, '.close, #fade', function(){
+//     popup(".active-popup");
+
+$("#the-archive-slideshow").flexslider({ animationLoop: false,slideshow: false,controlNav: false, prevText: "<<", nextText: ">>" });
+});
+//function popup(section) {
+//     $("#fade").fadeToggle();
+//     if ( $(section).hasClass("active-popup"){
+//         $(section).attr('aria-hidden',true).removeClass("active-popup");
+//         }
+//    else { $(section).attr('aria-hidden',false).addClass("active-popup");
+//}
